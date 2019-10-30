3ef8cff36f3147189d47a350ffb86d5fd46d2440
Danniel Rolfe <talkto@dannielrolfe.com>
2019-10-29 17:46:00 -0700

    WIP

--- app/auth.go
+++ app/auth.go
@@ -1,6 +1,9 @@
 package app
 
-import "net/http"
+import (
+	utils "rest-api-go/utils"
+	"net/http"
+)
 
 // JwtAuthentication middleware
 var JwtAuthentication = func(next http.Handler) {
@@ -13,7 +16,7 @@ var JwtAuthentication = func(next http.Handler) {
 		// current request path
 		requestPath := request.URL.Path
 
-		// Verify authentication
+		// Verify authentication on routes
 		// On succesful authentication send request
 		for _, route := range publicRoutes {
 			if route == requestPath {
@@ -21,6 +24,66 @@ var JwtAuthentication = func(next http.Handler) {
 				return
 			}
 		}
+
+		response := make(map[string]interface{})
+
+		// Get token from header
+		tokenHeader := request.Header.Get("Authorization")
+
+		// When token is missing return error code 403 unauthorized
+		if tokenHeader == "" {
+			response = utils.Message(false, "Missing auth token")
+			write.WriteHeader(http.StatusForbidden)
+			write.Header().add("Content-Type", "application/json")
+			utils.response(write, response)
+			return
+		}
+
+		// Reformat Bearer token
+		splitted := string.Split(tokenHeader, " ")
+		if len(splitted) != 2 {
+			reponse utils.Message(false, "Invalid or Malformed auth token")
+			write.WriteHeader(http.StatusForbidden)
+			write.Header().Add("Content-Type", "application/json")
+			utils.Respond(w, response)
+			return
+		}
+
+		// Get Token
+		tokenPart := splitted[1]
+		tk := &modles.Token{}
+
+		token, err := jwt.ParseWithClaims(token, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+		
+		// Malformed token
+		// Return 403
+		if err != nil {
+		 respone = utils.Messages(false, "Malformed authentication token")	
+		 write.WriteHeader(http.StatusForbidden)
+		 write.Header().Add("Content-Type", "application/json")
+		 utils.Responde(write, response)
+		 return
+		}
+
+		// Invalid token 
+		if !token.Valid {
+			response = utils.Message(false, "Token is not valid")
+			write.WriteHeader(http.StatusForbidden)
+			write.Header().Add("Content-Type", "application/json")
+			utils.Responde(write, response)
+			return
+		} 
+
+		// Valid Token 
+
+		// Log username
+		fmt.Sprintf("User %", tk.Username)
+		ctx := context.WithValue(response.Context, "user", tk.UserId)
+		response = response.WithContext(ctx)
+		next.ServeHttp(write, response)
+
 	})
 
 }
